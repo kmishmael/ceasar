eefd4f4a5e0abc484e1faf0f49852ce8666fa572
kibet <kmishmael@gmail.com>
2022-10-22 00:26:30 +0300

    use ASCII

--- ceasar.cpp
+++ ceasar.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
-#include <cctype>
 
 using namespace std;
  
-// function prototype
+/// @brief 
+/// @param k positive integer for the number of letters for displacement.
+/// @param plaintext  string text to be converted to cipher text.
+/// @return encrypted ciphertext string as per the input rules.
 string caesar(int k, string plaintext);
  
 int main(int argc, char* argv[])
 {
-    cout << argc << endl;
-    for(int j = 0; j < argc; j++){
-        cout << argv[j] << endl;
-    }
-   // TODO 1: declare an int to hold k as well as a string to hold the plaintext
+
+   //1: declare an int to hold k as well as a string to hold the plaintext
    int k;
    string plaintext;
  
-   // TODO 2: ensure that the right number of command line arguments are entered
+   // 2: ensure that the right number of command line arguments are entered
    // If the wrong number of arguments are entered exit by returning 3.
    // If the right number are entered assign k the appropriate value
 
@@ -27,40 +26,61 @@ int main(int argc, char* argv[])
     return 3;
    }
 
-   k = atoi(argv[0]);
+   k = atoi(argv[1]);
  
-   // TODO 3: read in the plaintext using getline
+   // 3: read in the plaintext using getline
    getline(cin, plaintext);
-   // TODO 4: run the function caesar with appropriate arguments and assign the value returned to a string called ciphertext
+   // 4: run the function caesar with appropriate arguments and assign the value returned to a string called ciphertext
    string ciphertext = caesar(k, plaintext);
    // Print out the ciphertext
-   cout << plaintext << " ";
-   cout << ciphertext;
+   cout << ciphertext << "\n";
+   return 0;
 }
  
-// TODO 5: implement the caesar function
-// Remember to take care of a case in which k is greater than 26
 string caesar(int k, string plaintext){
     
-    string letters = "abcdefghijklmnopqrstuvwxyz";
+    // initialise empty ciphertext variable.
     string ciphertext = "";
 
     for(int i = 0, n = plaintext.length(); i < n; i++)
     {
+        // set current character to currentCharacter variable
         char currentCharacter = plaintext[i];
 
-        if(isspace(plaintext[i])){
-            ciphertext.push_back(' ');
-            continue;
+        int alphaIndex;
+        int newIndex;
+        int newAsciiChar;
+
+       if(isalpha(currentCharacter)){
+
+        if(isupper(currentCharacter)){
+            // uppercase letter
+            // get the ASCII code of the uppercase letter, then get its index in alphabet
+            alphaIndex = int(currentCharacter) % 65;
+            newIndex = (alphaIndex + k) % 26;
+            newAsciiChar = 65 + newIndex;
+
+
+
+        }else {
+            // lower case letter
+            // get the ASCII code of the lowercase letter, then get its index in alphabet
+            alphaIndex = int(currentCharacter) % 97;
+            newIndex = (alphaIndex + k) % 26;
+            newAsciiChar = 97 + newIndex;
+            
         }
         
-        int currentIndex = letters.find(tolower(currentCharacter));
+        char newCharacter = char(newAsciiChar);
 
-        int newIndex = (currentIndex + k) % 26;
+        ciphertext += newCharacter;
 
-        char newCharacter = plaintext[newIndex];
+       } else {
 
-        ciphertext.push_back(((isupper(currentCharacter)) ? toupper(newCharacter) : newCharacter));
+        ciphertext += currentCharacter;
+
+       }
+      
     }
 
     return ciphertext;
